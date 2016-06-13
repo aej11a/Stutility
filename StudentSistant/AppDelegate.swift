ae24d50ff27b211ed62018148f8bc63bee58cb4b
Andrew Jones <aej11@hotmail.com>
2016-06-13 17:43:37 -0400

    :'(

--- StudentSistant/AppDelegate.swift
+++ StudentSistant/AppDelegate.swift
@@ -1,46 +1,55 @@
-//
-//  AppDelegate.swift
-//  StudentSistant
-//
-//  Created by Andrew Jones on 4/20/16.
-//  Copyright © 2016 Andrew Jones. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Sets up nav bar colors
+        UINavigationBar.appearance().barTintColor = UIColor(red: 41.0/255.0, green: 112.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.whiteColor()
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor()]
+        
+        // Sets up tab bar colors
+        UITabBar.appearance().barTintColor = UIColor(red: 41.0/255.0, green: 112.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+        UITabBar.appearance().tintColor = UIColor.whiteColor()
+        
+        /*
+         // Sets up selector colors
+         UISegmentedControl.appearance().backgroundColor = UIColor.whiteColor()
+         UISegmentedControl.appearance().tintColor = UIColor(red: 41.0/255.0, green: 112.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+         */
+        
+        // Sets status bar to white color, also requires UIViewControllerBasedStatusBarAppearance in info.plist be false
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
+        
         // Override point for customization after application launch.
         return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
-
